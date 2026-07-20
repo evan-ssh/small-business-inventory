3eff2ee1e06439472a43f377714d72e8b2b8512e
evan-ssh <evanhyde1@gmail.com>
2026-07-20 16:01:13 -0230

    Added Google Auth Utility functions
    -Added auth client setup
    
    -Added helper to genereate Google sign in URL
    
    -Added helper to fetch google users info
    
    -Added helper to find a user when logging in or create an user acc not found

---
+++ src/lib/googleOauthUtils.js
@@ -0,0 +1,60 @@
+import {google} from 'googleapis';
+import {getDB} from '@/lib/mongodb';
+
+const oauthclient = new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_SECRET,
+    process.env.GOOGLE_REDIRECT
+)
+
+export const getGoogleOauthUrl = () => {
+    const scopes = [
+        "https://www.googleapis.com/auth/userinfo.email",
+        "https://www.googleapis.com/auth/userinfo.profile",
+    ];
+    return oauthclient.generateAuthUrl({
+        access_type: "offline",
+        prompt: "consent",
+        scope: scopes
+    })
+}
+
+export async function getGoogleUser(code){
+    const {tokens} = await oauthclient.getToken(code);
+    const response = await fetch (
+        `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${tokens.access_token}`
+    )
+    const userInfo = await response.json();
+    return userInfo;
+}
+
+export async function updateOrCreateUserInfo(oauthUserInfo){
+    const db = await getDB();
+
+    const {id, email, name, picture} = oauthUserInfo;
+
+    let user = await db.collection('users').findOne({email});
+
+    if(!user){
+        const result = await db.collection("users").insertOne({
+            googleId:id,
+            email,
+            name,
+            picture
+        })
+
+    user = {
+        _id: result.insertedId,
+        googleId: id,
+        email,
+        name,
+        picture
+    };
+
+   
+    }
+    return user 
+}
+
+
+
